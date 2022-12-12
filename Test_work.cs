6ce27e7addf0e2a04e5c679cbc95aefe2d9dec3b
tchexp <tchexp@mail.ru>
2022-12-12 17:56:13 +0300

    First Local commit

---
+++ Test_work.cs
@@ -0,0 +1,17 @@
+﻿string[] InputArray()
+{
+    Console.WriteLine("Please input array size");
+    int arraySize = Convert.ToInt32(Console.ReadLine());
+    string[] newArray = new string[arraySize];
+    for (int i = 0; i < arraySize; i++)
+    {
+        Console.Write($"Please input array element #{i + 1} -> ");
+        newArray[i] = Console.ReadLine();
+    }
+    return newArray;
+}
+
+
+
+
+
