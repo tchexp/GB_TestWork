cbd0bc6ce0e318cc8d5ab84a589268dc664bc260
tchexp <tchexp@mail.ru>
2022-12-12 18:23:39 +0300

    Draft commit - 1

--- Test_work.cs
+++ Test_work.cs
@@ -11,7 +11,17 @@
     return newArray;
 }
 
+void PrintArray(string[] inputArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < inputArray.Length; i++)
+    {
+        Console.Write($"\"{inputArray[i]}\"");
+        if (i < inputArray.Length - 1) Console.Write(", ");
+    }
+    Console.Write("]");
+}
 
 
 
-
+PrintArray(InputArray());
